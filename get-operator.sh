4cc05826937bf02a619255b971c1945f8669d597
Roberto Scudeller <beto.rvs@gmail.com>
2020-03-10 07:45:34 +0100

    add more tests ; add script to download operator-sdk binary

---
+++ get-operator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# to be used in travis ci pipeline
+
+RELEASE_VERSION=v0.15.2
+
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+
+chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+
--- pkg/usecase/sensuclient_usecase_test.go
+++ pkg/usecase/sensuclient_usecase_test.go
@@ -23,8 +23,26 @@ var (
 	AddNewMemberCalls                 int
 	RemoveMemberCalls                 int
 	AddResourceCalls                  int
-	DeleteResourceCalls               int
+	AddAssetsCalls                    int
+	AddNamespacesCalls                int
+	AddChecksCalls                    int
+	AddHandlersCalls                  int
+	AddFiltersCalls                   int
+	AddMutatorsCalls                  int
 	CheckResourceExistCalls           int
+	CheckAssetsExistCalls             int
+	CheckNamespacesExistCalls         int
+	ChecksExistCalls                  int
+	CheckHandlersExistCalls           int
+	CheckFiltersExistCalls            int
+	CheckMutatorsExistCalls           int
+	DeleteResourceCalls               int
+	DeleteAssetsCalls                 int
+	DeleteNamespacesCalls             int
+	DeleteChecksCalls                 int
+	DeleteHandlersCalls               int
+	DeleteFiltersCalls                int
+	DeleteMutatorsCalls               int
 )
 
 type SensuRepositoryMock struct {
@@ -71,15 +89,63 @@ func (repo SensuRepositoryMock) RemoveMember(sensuurl string, token string, memb
 	return nil
 }
 func (repo SensuRepositoryMock) CheckResourceExist(sensuurl string, token string, resource string, namespace string, name string) bool {
-	CheckResourceExistCalls++
+	switch resource {
+	case "assets":
+		CheckAssetsExistCalls++
+	case "namespaces":
+		CheckNamespacesExistCalls++
+	case "checks":
+		ChecksExistCalls++
+	case "handlers":
+		CheckHandlersExistCalls++
+	case "filters":
+		CheckFiltersExistCalls++
+	case "mutators":
+		CheckMutatorsExistCalls++
+	default:
+		CheckResourceExistCalls++
+	}
+
 	return true
 }
 func (repo SensuRepositoryMock) AddResource(sensuurl string, token string, resource string, namespace string, bodymarshal []byte) error {
-	AddResourceCalls++
+
+	switch resource {
+	case "assets":
+		AddAssetsCalls++
+	case "namespaces":
+		AddNamespacesCalls++
+	case "checks":
+		AddChecksCalls++
+	case "handlers":
+		AddHandlersCalls++
+	case "filters":
+		AddFiltersCalls++
+	case "mutators":
+		AddMutatorsCalls++
+	default:
+		AddResourceCalls++
+	}
 	return nil
 }
 func (repo SensuRepositoryMock) DeleteResource(sensuurl string, token string, namespace string, resource string, name string) error {
-	DeleteResourceCalls++
+	switch resource {
+	case "assets":
+		DeleteAssetsCalls++
+	case "namespaces":
+		DeleteNamespacesCalls++
+	case "checks":
+		DeleteChecksCalls++
+	case "handlers":
+		DeleteHandlersCalls++
+	case "filters":
+		DeleteFiltersCalls++
+	case "mutators":
+		DeleteMutatorsCalls++
+	default:
+		DeleteResourceCalls++
+	}
+
 	return nil
 }
 
@@ -181,8 +247,58 @@ func TestCheckAssetExist(t *testing.T) {
 	appcontext.Current.Add(appcontext.SensuRepository, repo)
 	_ = CheckAssetExist(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
 	expected := 1
-	if CheckResourceExistCalls != expected {
-		t.Fatalf("Invalid 11.1 TestCheckAssetExist %d", CheckResourceExistCalls)
+	if CheckAssetsExistCalls != expected {
+		t.Fatalf("Invalid 11.1 TestCheckAssetExist %d", CheckAssetsExistCalls)
+	}
+}
+
+func TestCheckExist(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = CheckExist(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if ChecksExistCalls != expected {
+		t.Fatalf("Invalid 11.2 TestCheckExist %d", ChecksExistCalls)
+	}
+}
+
+func TestCheckNamespaceExist(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = CheckNamespaceExist(fakeSensuURL, "aaaaaqqqqqwwww", "default")
+	expected := 1
+	if CheckNamespacesExistCalls != expected {
+		t.Fatalf("Invalid 11.3 TestCheckNamespaceExist %d", CheckNamespacesExistCalls)
+	}
+}
+
+func TestCheckHandlerExist(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = CheckHandlerExist(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if CheckHandlersExistCalls != expected {
+		t.Fatalf("Invalid 11.4 TestHandlerAssetExist %d", CheckHandlersExistCalls)
+	}
+}
+
+func TestCheckFilterExist(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = CheckFilterExist(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if CheckFiltersExistCalls != expected {
+		t.Fatalf("Invalid 11.5 TestCheckFilterExist %d", CheckFiltersExistCalls)
+	}
+}
+
+func TestCheckMutatorExist(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = CheckMutatorExist(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if CheckMutatorsExistCalls != expected {
+		t.Fatalf("Invalid 11.6 TestCheckMutatorExist %d", CheckMutatorsExistCalls)
 	}
 }
 
@@ -199,8 +315,93 @@ func TestAddAsset(t *testing.T) {
 	}
 	_ = AddAsset(fakeSensuURL, "aaaaaqqqqqwwww", "default", sensuAsset)
 	expected := 1
-	if AddResourceCalls != expected {
-		t.Fatalf("Invalid 12.1 TestAddAsset %d", AddResourceCalls)
+	if AddAssetsCalls != expected {
+		t.Fatalf("Invalid 12.1 TestAddAsset %d", AddAssetsCalls)
+	}
+}
+
+func TestAddNamespace(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = AddNamespace(fakeSensuURL, "aaaaaqqqqqwwww", "test")
+	expected := 1
+	if AddNamespacesCalls != expected {
+		t.Fatalf("Invalid 12.2 TestAddNamespace %d", AddNamespacesCalls)
+	}
+}
+
+func TestAddCheck(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	sensuCheck := &v2.Check{
+		Subscriptions: []string{"test"},
+		Command:       "echo",
+		Interval:      uint32(60),
+		Publish:       true,
+		Handlers:      []string{"test"},
+		ObjectMeta: v2.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+	_ = AddCheck(fakeSensuURL, "aaaaaqqqqqwwww", "default", sensuCheck)
+	expected := 1
+	if AddChecksCalls != expected {
+		t.Fatalf("Invalid 12.3 TestAddChecks %d", AddChecksCalls)
+	}
+}
+
+func TestAddHandler(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	sensuHandler := &v2.Handler{
+		Type:     "pipe",
+		Command:  "echo",
+		Handlers: []string{"test"},
+		ObjectMeta: v2.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+	_ = AddHandler(fakeSensuURL, "aaaaaqqqqqwwww", "default", sensuHandler)
+	expected := 1
+	if AddHandlersCalls != expected {
+		t.Fatalf("Invalid 12.4 TestAddHandler %d", AddHandlersCalls)
+	}
+}
+
+func TestAddFilter(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	sensuFilter := &v2.EventFilter{
+		Action:      "allow",
+		Expressions: []string{"event.entity.labels['environment'] == 'production'"},
+		ObjectMeta: v2.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+	_ = AddFilter(fakeSensuURL, "aaaaaqqqqqwwww", "default", sensuFilter)
+	expected := 1
+	if AddFiltersCalls != expected {
+		t.Fatalf("Invalid 12.5 TestAddFilter %d", AddFiltersCalls)
+	}
+}
+
+func TestAddMutator(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	sensuMutator := &v2.Mutator{
+		Command: "echo",
+		ObjectMeta: v2.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+	}
+	_ = AddMutator(fakeSensuURL, "aaaaaqqqqqwwww", "default", sensuMutator)
+	expected := 1
+	if AddMutatorsCalls != expected {
+		t.Fatalf("Invalid 12.6 TestAddMutator %d", AddMutatorsCalls)
 	}
 }
 
@@ -209,7 +410,57 @@ func TestDeleteAsset(t *testing.T) {
 	appcontext.Current.Add(appcontext.SensuRepository, repo)
 	_ = DeleteAsset(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
 	expected := 1
-	if DeleteResourceCalls != expected {
-		t.Fatalf("Invalid 13.1 TestDeleteAsset %d", DeleteResourceCalls)
+	if DeleteAssetsCalls != expected {
+		t.Fatalf("Invalid 13.1 TestDeleteAsset %d", DeleteAssetsCalls)
+	}
+}
+
+func TestDeleteNamespace(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = DeleteNamespace(fakeSensuURL, "aaaaaqqqqqwwww", "default")
+	expected := 1
+	if DeleteNamespacesCalls != expected {
+		t.Fatalf("Invalid 13.2 TestDeleteNamespace %d", DeleteNamespacesCalls)
+	}
+}
+
+func TestDeleteCheck(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = DeleteCheck(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if DeleteChecksCalls != expected {
+		t.Fatalf("Invalid 13.3 TestDeleteCheck %d", DeleteChecksCalls)
+	}
+}
+
+func TestDeleteHandler(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = DeleteHandler(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if DeleteHandlersCalls != expected {
+		t.Fatalf("Invalid 13.4 TestDeleteHandler %d", DeleteHandlersCalls)
+	}
+}
+
+func TestDeleteFilter(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = DeleteFilter(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if DeleteFiltersCalls != expected {
+		t.Fatalf("Invalid 13.5 TestDeleteFilter %d", DeleteFiltersCalls)
+	}
+}
+
+func TestDeleteMutator(t *testing.T) {
+	repo := SensuRepositoryMock{}
+	appcontext.Current.Add(appcontext.SensuRepository, repo)
+	_ = DeleteMutator(fakeSensuURL, "aaaaaqqqqqwwww", "default", "test")
+	expected := 1
+	if DeleteMutatorsCalls != expected {
+		t.Fatalf("Invalid 13.6 TestDeleteMutator %d", DeleteMutatorsCalls)
 	}
 }
